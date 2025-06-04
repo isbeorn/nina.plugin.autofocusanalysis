1ce185d019a2ded2041d72096638a0bf2c773d0b
Stefan Berg <isbeorn86@googlemail.com>
2025-06-04 08:37:59 +0200

    2.0.0.8

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Runtime.InteropServices;
 
 // [MANDATORY] The assembly versioning
 //Should be incremented for each new release build of a plugin
-[assembly: AssemblyVersion("2.0.0.2")]
-[assembly: AssemblyFileVersion("2.0.0.2")]
+[assembly: AssemblyVersion("2.0.0.8")]
+[assembly: AssemblyFileVersion("2.0.0.8")]
 
 // [MANDATORY] The name of your plugin
 [assembly: AssemblyTitle("Autofocus Report Analysis")]
